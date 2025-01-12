6937acd17ffec9f2ddb9c2567fd7cdc4b0fbedbb
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-01-12 21:19:08 +0530

    Add dialog box which will open on clicking the card
    This dialog box will display complete  details and contain purchase button

--- lib/home.dart
+++ lib/home.dart
@@ -4,8 +4,10 @@ import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
 import 'package:sidebarx/sidebarx.dart';
 import 'dart:ui' as ui;
 import 'package:circle_nav_bar/circle_nav_bar.dart';
+import 'package:wolt_modal_sheet/wolt_modal_sheet.dart';
 
-Future<void> preloadImages(BuildContext context, List<String> imagePaths) async {
+Future<void> preloadImages(BuildContext context,
+    List<String> imagePaths) async {
   for (var imagePath in imagePaths) {
     await precacheImage(AssetImage(imagePath), context);
   }
@@ -22,12 +24,19 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
 
   List<Item> data = [
-    Item(name: 'Badminton', imgUrl: 'assets/images/badminton.jpg', description: 'In Good Working Conditions', rentStatus: true, saleStatus: false, price: 400.0)
+    Item(name: 'Badminton',
+        imgUrl: 'assets/images/badminton.jpg',
+        description: 'In Good Working Conditions',
+        rentStatus: true,
+        saleStatus: false,
+        price: 400.0)
   ];
   List<String> carouselImages = ['coca-cola', 'sprite', 'drinks', 'oreo-shake'];
 
-  int _tabIndex = 1;
+  int _tabIndex = 2;
+
   int get tabIndex => _tabIndex;
+
   set tabIndex(int v) {
     _tabIndex = v;
     setState(() {});
@@ -51,8 +60,6 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amberAccent,
@@ -67,121 +74,200 @@ class _HomeState extends State<Home> {
         ),
       ),
       endDrawer: SidebarX(
-        controller: SidebarXController(selectedIndex: 0, extended: true),
-        items: [
-          SidebarXItem(icon: Icons.home, label: 'Home', onTap: () { print('Home is clicked'); }),
-          SidebarXItem(icon: Icons.search, label: 'Search'),
-        ]
+          controller: SidebarXController(selectedIndex: 0, extended: true),
+          items: [
+            SidebarXItem(icon: Icons.home, label: 'Home', onTap: () {
+              print('Home is clicked');
+            }),
+            SidebarXItem(icon: Icons.search, label: 'Search'),
+          ]
       ),
       body: PageView(
           controller: pageController,
           onPageChanged: (v) {
             tabIndex = v;
           },
-        children: [Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Container(
-                  child: FlutterCarousel(
-                    options: FlutterCarouselOptions(
-                      height: 400.0,
-                      showIndicator: true,
-                      slideIndicator: CircularSlideIndicator(),
-                      autoPlay: true,
-                      autoPlayInterval: Duration(seconds: 3),
-                      viewportFraction: 1.0,
-                      enlargeCenterPage: true,
-                    ),
-                    items: carouselImages.map((i) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Container(
-                              width: MediaQuery.of(context).size.width,
-                              margin: EdgeInsets.symmetric(horizontal: 5.0),
-                              decoration: BoxDecoration(
-                                  color: Colors.amber
-                              ),
-                              child: Image(
-                                image: AssetImage('assets/images/$i.jpg'),
-                                fit: BoxFit.cover,
-                              ),
+          children: [Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Container(
+                      child: FlutterCarousel(
+                        options: FlutterCarouselOptions(
+                          height: 400.0,
+                          showIndicator: true,
+                          slideIndicator: CircularSlideIndicator(),
+                          autoPlay: true,
+                          autoPlayInterval: Duration(seconds: 3),
+                          viewportFraction: 1.0,
+                          enlargeCenterPage: true,
+                        ),
+                        items: carouselImages.map((i) {
+                          return Builder(
+                            builder: (BuildContext context) {
+                              return Container(
+                                width: MediaQuery
+                                    .of(context)
+                                    .size
+                                    .width,
+                                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                                decoration: BoxDecoration(
+                                    color: Colors.amber
+                                ),
+                                child: Image(
+                                  image: AssetImage('assets/images/$i.jpg'),
+                                  fit: BoxFit.cover,
+                                ),
+                              );
+                            },
                           );
-                        },
-                      );
-                    }).toList(),
-                  )
-              ),
-              SizedBox(height: 30.0,),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: data.length,
-                  itemBuilder: (context, index) {
+                        }).toList(),
+                      )
+                  ),
+                  SizedBox(height: 30.0,),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        String rentMsg = data[index].rentStatus
+                            ? 'On Rent'
+                            : 'Not On Rent';
+                        String saleMsg = data[index].saleStatus
+                            ? 'On Sale'
+                            : 'Not On Sale';
 
-                    String rentMsg = data[index].rentStatus ? 'On Rent' : 'Not On Rent';
-                    String saleMsg = data[index].saleStatus ? 'On Sale' : 'Not On Sale';
+                        return Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 1.0, horizontal: 4.0),
+                          child: Container(
+                            height: 110,
+                            child: GestureDetector(
+                              onTap: () {
+                                print(data[index].name);
+                                showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: Center(
+                                          child: Text(
+                                              data[index].name
+                                          ),
+                                        ),
+                                        content: Builder(
+                                          builder: (context) {
+                                            return Container(
+                                              height: 300,
+                                              child: Column(
+                                                children: [
+                                                  Container(
+                                                    child: Image.asset(
+                                                      data[index].imgUrl,
+                                                      height: 100,
+                                                      fit: BoxFit.cover,
+                                                    ),
+                                                  ),
+                                                  SizedBox(height: 20.0,),
+                                                  Text(
+                                                    rentMsg,
+                                                  ),
 
-                    return Padding(
-                      padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
-                      child: Container(
-                        height: 110,
-                        child: GestureDetector(
-                          onTap: () {
-                            print(data[index].name);
-                          },
-                          child: Card(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  CircleAvatar(
-                                    backgroundImage: AssetImage(data[index].imgUrl),
-                                  ),
-                                  Column(
+                                                  Text(
+                                                    saleMsg,
+                                                  ),
+
+                                                  Text(
+                                                    '${data[index].price} INR',
+                                                  ),
+                                                  SizedBox(height: 20.0,),
+
+                                                  Text(
+                                                    data[index].description,
+                                                  )
+                                                ],
+                                              ),
+                                            );
+                                          }
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: Text(
+                                              'Cancel',
+                                            ),
+                                          ),
+                                          ElevatedButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: Text(
+                                              'Purchase',
+                                            ),
+                                            style: ElevatedButton.styleFrom(
+                                              backgroundColor: Colors.amberAccent,
+                                            ),
+                                          )
+                                        ],
+                                      );
+                                    }
+                                );
+                              },
+                              child: Card(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment
+                                        .spaceBetween,
                                     children: [
-                                      Text(
-                                        data[index].name,
+                                      CircleAvatar(
+                                        backgroundImage: AssetImage(
+                                            data[index].imgUrl),
                                       ),
-                                      Text(
-                                        rentMsg,
+                                      Column(
+                                        children: [
+                                          Text(
+                                            data[index].name,
+                                          ),
+                                          Text(
+                                            rentMsg,
+                                          ),
+                                          Text(
+                                            saleMsg,
+                                          ),
+                                          Text(
+                                            '${data[index].price} INR',
+                                          )
+                                        ],
                                       ),
-                                      Text(
-                                        saleMsg,
-                                      ),
-                                      Text(
-                                        '${data[index].price} INR',
+
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Container(
+                                          child: Column(
+                                            children: [
+                                              Text(
+                                                data[index].description,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                            ],
+                                          ),
+                                        ),
                                       )
                                     ],
                                   ),
-
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Container(
-                                      child: Column(
-                                        children: [
-                                          Text(
-                                            data[index].description,
-                                            overflow: TextOverflow.ellipsis,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  )
-                                ],
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
+                        );
+                      },
+                    ),
+                  )
+                ],
               )
-            ],
-          )
-        ),
-    ]
+          ),
+          ]
       ),
       bottomNavigationBar: CircleNavBar(
         activeIcons: const [
