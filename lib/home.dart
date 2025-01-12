fa49996ef25ae811e77888f78db9bf22aa50032d
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2025-01-12 12:00:25 +0530

    Add carousel for the images in the homepage

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:demo_app_rr/item.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
 import 'package:sidebarx/sidebarx.dart';
+import 'dart:ui' as ui;
+
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -14,9 +17,22 @@ class _HomeState extends State<Home> {
   List<Item> data = [
     Item(name: 'Badminton', imgUrl: 'assets/images/badminton.jpg', description: 'In Good Working Conditions', rentStatus: true, saleStatus: false, price: 400.0)
   ];
+  List<String> carouselImages = ['coca-cola', 'sprite', 'drinks', 'oreo-shake'];
+
+
 
   @override
   Widget build(BuildContext context) {
+
+    @override
+    void initState() {
+      // TODO: implement initState
+      super.initState();
+      for(String imageName in carouselImages){
+        precacheImage('assets/images/$imageName.jpg' as ImageProvider<Object>, context);
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amberAccent,
@@ -42,8 +58,36 @@ class _HomeState extends State<Home> {
         child: Column(
           children: [
             Container(
-              height: 20.0,
+                child: FlutterCarousel(
+                  options: FlutterCarouselOptions(
+                    height: 400.0,
+                    showIndicator: true,
+                    slideIndicator: CircularSlideIndicator(),
+                    autoPlay: true,
+                    autoPlayInterval: Duration(seconds: 3),
+                    viewportFraction: 1.0,
+                    enlargeCenterPage: true,
+                  ),
+                  items: carouselImages.map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            decoration: BoxDecoration(
+                                color: Colors.amber
+                            ),
+                            child: Image(
+                              image: AssetImage('assets/images/$i.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                        );
+                      },
+                    );
+                  }).toList(),
+                )
             ),
+            SizedBox(height: 30.0,),
             Expanded(
               child: ListView.builder(
                 itemCount: data.length,
@@ -57,6 +101,9 @@ class _HomeState extends State<Home> {
                     child: Container(
                       height: 110,
                       child: GestureDetector(
+                        onTap: () {
+                          print(data[index].name);
+                        },
                         child: Card(
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
@@ -112,3 +159,5 @@ class _HomeState extends State<Home> {
     );
   }
 }
+
+
